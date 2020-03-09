f6c54efe7b703888b85de1016d715bda8654d8de
Megan Shaw <meganrose@Megans-MacBook-Air.local>
2020-03-09 15:42:53 -0400

    Adding server logic

---
+++ server.js
@@ -0,0 +1,16 @@
+// setting package requirements
+var express = require("express");
+var app = express();
+var path = require("path");
+
+// setting port requirements
+var PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+// include the routes
+require("./routes/index.js")(app);
+
+app.listen(PORT, function () {
+    console.log("Server listening on port " + PORT);
+});
