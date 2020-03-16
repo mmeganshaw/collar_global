541987b82bbd7cc3096a868ff66aa84b7e11f730
Megan Shaw <meganrose@Megans-MacBook-Air.local>
2020-03-16 15:34:10 -0400

    Last add before deploy.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.static(__dirname + '/public'));
 // include the routes
 require("./routes/index.js")(app);
 
+
 app.listen(PORT, function () {
     console.log("Server listening on port " + PORT);
 });
